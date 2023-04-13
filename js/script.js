e677ae34135c363ba48dfae801cbd0704fd4f260
Laptev S <sl260401@gmail.com>
2023-04-13 10:43:32 +0300

    v1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
 "use strict"
+
+let index = 0;
+
+let images = document.getElementsByClassName("img");
+let dots = document.getElementsByClassName("dot");
+
+showImage(index);
+
+function showImage(i) {
+	index += i;
+
+
+	for (i = 0; i < images.length; i++) {
+		images[i].style.display = "none";
+	}
+
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+
+	if (index > images.length - 1) {
+		index = 0;
+	}
+
+	if (index < 0) {
+		index = images.length - 1;
+	}
+
+	images[index].style.display = "block";
+	dots[index].className += " active";
+}
